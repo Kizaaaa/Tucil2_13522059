e07db38fddf4061e1d3419c68dc893a76ae9a4dc
Kiza <themightyomegagd@gmail.com>
2024-03-19 07:43:12 +0700

    dnc

---
+++ src/dnc.py
@@ -0,0 +1,58 @@
+import matplotlib.pyplot as plt
+import time
+
+# Fungsi untuk mendapatkan titik tengah dari 2 buah titik
+def getMidPoint(p1,p2):
+    return [(p1[0]+p2[0])/2,(p1[1]+p2[1])/2]
+
+
+# Fungsi untuk menghasilkan titik tengah diantara titik kontrol kemudian
+# mencari lagi titik tengahnya secara rekursif
+def getPoints(points):
+    if len(points) <= 1 :
+        return points
+    else :
+        ret = [points[0]]
+        temp = []
+        for i in range(len(points) - 1) :
+            temp.append(getMidPoint(points[i],points[i+1]))
+        ret.extend(getPoints(temp))
+        ret.append(points[-1])
+        return ret
+
+
+# Fungsi Divide and Conquer
+def divideAndConquer(points, iterate):
+    if iterate == 0 :
+        return []
+    else :
+        points = getPoints(points)
+        iterate -= 1
+        idxMid = len(points)//2
+        return divideAndConquer(points[0:idxMid+1],iterate) + [points[idxMid]] + divideAndConquer(points[idxMid:],iterate)
+    
+def DNCmain(points,iterate) :
+    # Mulai waktu perhitungan
+    start = time.time()
+
+    # Mulai program Divide and Conquer
+    pointAkhir = []
+    pointAkhir.append(points[0])
+    pointAkhir.extend(divideAndConquer(points,iterate))
+    pointAkhir.append(points[-1])
+
+    # Stop waktu perhitungan
+    end = time.time()
+
+    # Menghitung lama waktu yang diperlukan
+    totalWaktu = end - start
+
+    for i in range(len(points)-1) :
+        plt.plot([points[i][0], points[i+1][0]], [points[i][1], points[i+1][1]], marker='o', linestyle='-', color="black")
+    for i in range(len(pointAkhir)-1) :
+        plt.plot([pointAkhir[i][0], pointAkhir[i+1][0]], [pointAkhir[i][1], pointAkhir[i+1][1]], marker='o', linestyle='-', color="black")
+
+    plt.text(0,0,f"Waktu yang diperlukan: {totalWaktu:.4f} detik")
+
+    plt.grid(True)
+    plt.show()
--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from dnc import *
 
 # Fungsi untuk mengecek apakah input adalah float yang valid atau tidak
 def isFloat(strings):
@@ -10,49 +11,32 @@ def isFloat(strings):
     else:
         return False
 
-# Fungsi untuk mendapatkan titik tengah dari 2 titik
-def getMiddle(x1,x2,y1,y2):
-    return (x1+x2)/2,(y1+y2)/2
-
-# Fungsi Divide and Conquer
-def divideAndConquer():
-    pass
-
 # Input banyak titik
 inputs = input("Masukkan banyak titik (minimal 3) : ")
 while not inputs.isdigit() or int(inputs) < 3 :
     print("Masukan tidak valid!")
     inputs = input("Masukkan banyak titik (minimal 3) : ")
 
-points = int(inputs)
+manyPoint = int(inputs)
 
 # Input titik
-xArray = []
-yArray = []
+points = []
 
-for i in range(1,points+1):
+for i in range(1,manyPoint+1):
     print(f"Masukkan titik ke-{i} (x y) : ", end="")
     inputs = list(map(str,input().split()))
     while len(inputs) != 2 or not isFloat(inputs[0]) or not isFloat(inputs[1]) :
         print("Masukan tidak valid!")
         print(f"Masukkan titik ke-{i} (x y) : ", end="")
         inputs = list(map(str,input().split()))
-    xArray.append(float(inputs[0]))
-    yArray.append(float(inputs[1]))
+    points.append([float(inputs[0]),float(inputs[1])])
 
 # Input banyak iterasi
-
 inputs = input("Masukkan banyak iterasi (minimal 1) : ")
 while not inputs.isdigit() or int(inputs) < 1  :
     print("Masukan tidak valid!")
     inputs = input("Masukkan banyak iterasi (minimal 1) : ")
 
-points = int(inputs)
+iterasi = int(inputs)
 
-
-
-# xPoints = np.array(xArray)
-# yPoints = np.array(yArray)
-
-# plt.plot(xPoints, yPoints)
-# plt.show()
+DNCmain(points,iterasi)
